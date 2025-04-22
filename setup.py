88c88baa3af39db40c83bd27f4b98f609feb44d8
Juliano Benvenuto Piovezan <juliano.piovezan@asaas.com.br>
2025-04-22 10:36:42 -0300

    Atualização setup.py.

--- setup.py
+++ setup.py
@@ -1,15 +1,4 @@
-#!/usr/bin/env python
 from setuptools import setup, find_packages
-try:  # for pip >= 10
-    from pip._internal.req import parse_requirements as parse
-except ImportError:  # for pip <= 9.0.3
-    from pip.req import parse_requirements as parse
-
-def requirements(f):
-    try:
-        return [str(i.req) for i in parse(f, session=False)]
-    except:
-        return [str(i.requirement) for i in parse(f, session=False)]
 
 setup(
     name='PyNFe',
@@ -18,6 +7,11 @@ setup(
     package_data={
         'pynfe': ['data/cert/*.cer'],
     },
-    install_requires=requirements('requirements.txt'),
+    install_requires=[
+        "pyopenssl",
+        "requests",
+        "lxml",
+        "signxml",
+    ],
     zip_safe=False,
 )
