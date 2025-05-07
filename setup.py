2d211a76a1934454f5a13faa24d762b8c1cfec98
Juliano Benvenuto Piovezan <juliano.piovezan@asaas.com.br>
2025-05-07 13:30:48 -0300

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PyNFe',
-    version='0.10',
+    version='0.11',
     packages=find_packages(),
     package_data={
         'pynfe': ['data/cert/*.cer'],
